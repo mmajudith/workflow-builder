82e0b0a196057c0e924e1bdc23d46691608ad8b4
mma judith <mmajudith2000@gmail.com>
2021-09-17 23:29:48 -0700

    Workflow builder

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Route, Link } from 'react-router-dom';
+import { Route, Link, Switch } from 'react-router-dom';
 
 import { ActionsNode } from './reactFlow/ActionsNode';
 import { EmailActionNode } from './reactFlow/EmailActionNode';
@@ -10,7 +10,6 @@ function App() {
   return (
     <div className="App">
       <header className="nav-header">
-        <div>
           <ul className="items-wrapper">
               <li>Home</li>
               <li style={{borderBottom: '5px solid hsl(335, 100%, 69%)'}}>
@@ -19,13 +18,14 @@ function App() {
               <li>Performance</li>
               <li>History</li>
           </ul>
-        </div>
       </header>
 
-      <Route exact path="/workflow-builder" component={ActionsNode}/>
-      <Route exact path="/workflow-builder/email" component={EmailActionNode}/>
-      <Route exact path="/workflow-builder/schedule" component={ScheduleActionNode}/>
-      <Route exact path="/workflow-builder/reminder" component={ReminderActionNode}/>
+      <Switch>
+        <Route exact path="/workflow-builder" component={ActionsNode}/>
+        <Route exact path="/workflow-builder/email" component={EmailActionNode}/>
+        <Route exact path="/workflow-builder/schedule" component={ScheduleActionNode}/>
+        <Route exact path="/workflow-builder/reminder" component={ReminderActionNode}/>
+      </Switch>
     </div>
   );
 }
